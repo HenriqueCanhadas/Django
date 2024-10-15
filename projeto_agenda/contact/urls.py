66ddc692520459d48f7006a4a7c9850dd898aae9
Henrique Canhadas <pedrosacanhadas@gmail.com>
2024-10-15 15:56:45 -0300

    att aula 473

--- projeto_agenda/contact/urls.py
+++ projeto_agenda/contact/urls.py
@@ -5,5 +5,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
--- projeto_agenda/contact/views/contact_views.py
+++ projeto_agenda/contact/views/contact_views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context ={
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos -',
     }
 
@@ -30,5 +37,41 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context
+    )  
+
+# Create your views here.
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    print(search_value)
+
+    contacts = Contact.objects\
+            .filter(show=True)\
+            .filter(
+                Q(primeiro_nome__contains=search_value)
+                |
+                Q(ultimo_nome__contains=search_value)
+            )\
+            .order_by('-id')\
+            
+    print(contacts.query)
+
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+
+    context ={
+        'page_obj': page_obj,
+        'site_title': 'Search -',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context
     )  
